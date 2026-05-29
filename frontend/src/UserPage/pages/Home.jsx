e5833df3381ce7812c9db2fcceb6ddbb2183884f
praveen6235 <praveenbollam9550@gmail.com>
2026-05-29 12:06:13 +0530

    msg

--- frontend/src/UserPage/pages/Home.jsx
+++ frontend/src/UserPage/pages/Home.jsx
@@ -48,7 +48,7 @@ export default function Home() {
 
               {/* Headline */}
               <h1 style={{ fontSize: "clamp(2.1rem,5vw,3.4rem)", fontWeight: 900, lineHeight: 1.12, marginBottom: "1.1rem" }}>
-                Home Services<br />
+                Home Services Services Services<br />
                 <span className="g-text">At Your Doorstep</span>
               </h1>
 
